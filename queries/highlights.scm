f421bbf593a78267c27d6c7a813242c173999bca
Darius <dariuscorvus@pm.me>
2023-04-18 20:46:38 +0200

    feat: keywords

---
+++ queries/highlights.scm
@@ -0,0 +1,7 @@
+(keyword_define) @keyword
+(keyword_namespace) @keyword
+(keyword_database) @keyword
+(keyword_login) @keyword
+(keyword_table) @keyword
+(keyword_on) @keyword
+(semicolon) @operator
