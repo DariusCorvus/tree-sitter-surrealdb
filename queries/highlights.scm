6125c4b6d1af7b84c19439cc372bb71cd7b88484
Darius <dariuscorvus@pm.me>
2024-02-20 18:26:30 +0100

    feat: updated query

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,18 +1,19 @@
-
-(keyword) @keyword
-(string) @string
-(number) @number
-(punctuation) @punctuation 
-(operator) @operator
-(variable) @type
-(constant) @constant.builtin
-(function) @function
-(bool) @boolean
-(nothing) @type
-(comment) @comment
-(record) @type
-(function) @function
-(property) @field
-(identifier) @identifier
-(casting) @conceal
-(duration) @number
+	(keyword) @keyword
+	(string) @string
+	(number) @number
+	(punctuation) @punctuation
+	(operator) @operator
+	(variable) @type
+	(constant) @constant.builtin
+	(function) @function
+	(bool) @boolean
+	(nothing) @type
+	(comment) @comment
+	(record) @type
+	(function) @function
+	(property) @field
+	(identifier) @constant @text.emphasis (#set! "priority" 120)
+	(casting) @conceal
+	(duration) @number
+	(type) @type
+	(delimiter) @punctuation.delimiter
